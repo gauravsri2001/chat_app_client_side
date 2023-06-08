86de271cd886f1ed5ac54d55d5c68da69f05f7e1
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-06-08 19:15:27 +0530

    add

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -5,8 +5,12 @@ import ChannelSearch from './ChannelSearch';
 import { ChannelList } from 'stream-chat-react';
 import TeamChannelList from './TeamChannelList';
 import TeamChannelPreview from './TeamChannelPreview';
+import Cookies from 'universal-cookie';
 
-const SideBar = () => (
+
+const cookies = new Cookies();
+
+const SideBar = ({logout}) => (
     <div className='channel-list_sidebar'>
 
     <div className='channel-list_sidebar_icon1'>
@@ -16,7 +20,7 @@ const SideBar = () => (
     </div>
 
     <div className='channel-list_sidebar_icon1'>
-        <div className='icon1_inner'>
+        <div className='icon1_inner' onClick={logout}>
             <img src={Log_Out} alt='Logout' width='30' />
         </div>
     </div>
@@ -32,9 +36,21 @@ const CompanyHeader = () => (
 )
 
 const ChannelListContainer = () => {
+    const logout = () => {
+        cookies.remove('token');
+        cookies.remove('userId');
+        cookies.remove('username');
+        cookies.remove('fullName'); 
+        cookies.remove('phoneNumber'); 
+        cookies.remove('avatarURL'); 
+        cookies.remove('hashedPassword');
+
+        window.location.reload();
+    }
+
   return (
     <>
-        <SideBar />
+        <SideBar logout={logout} />
         <div className='channel-list__list__wrapper'>
             <CompanyHeader />
             <ChannelSearch />
